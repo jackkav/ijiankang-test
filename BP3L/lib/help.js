e070bb0feddd7de5ab7b501e95b10a51ff5f7800
Karose Liu <kusoroselh@gmail.com>
2016-05-12 23:37:20 +0800

    Fixed device

--- BP3L/lib/help.js
+++ BP3L/lib/help.js
@@ -1,7 +1,7 @@
 
 h.getDeviceInfo = ()=> {
 
-  if (!window.device) return true //浏览器 暂不考虑
+  if (!window.device) return {} //浏览器 暂不考虑
 
   // let platform = window.device.platform
   // let model = window.device.model
@@ -9,6 +9,6 @@ h.getDeviceInfo = ()=> {
   //
   // let stringFiy = `设备型号 ${platform}; 设备品牌${model}; 设备版本${version}`
 
-  return  window.device
+  return  {...window.device}
 
 }
