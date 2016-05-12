35c250fd10052aaf223dd8a7454074eb1b886ecd
Jeffrey Fan <ennea8@gmail.com>
2016-05-12 23:28:19 +0800

    Add mogodb env

---
+++ BP3L/env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#export FRAMEWORK_LIB=~/Desktop/ihealth/framework-iHealth/library
+
+export BUILD_ROOT=`pwd`
+
+
+export MONGO_URL=mongodb://deviceLoadTestByiHealth:djsDfjweFHp33ew@120.131.8.26:27017,120.131.8.27:27017,120.131.8.46:27017/device-load-test?replicaSet=iHealthMongo
+export MONGO_OPLOG_URL=mongodb://deviceLoadTestByiHealth:djsDfjweFHp33ew@120.131.8.26:27017,120.131.8.27:27017,120.131.8.46:27017/local?authSource=device-load-test
+
+echo $PACKAGE_DIRS
