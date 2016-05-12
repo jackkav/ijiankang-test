b392c758bb37e2cc8f96f52398cf554d46db54b6
Jeffrey Fan <ennea8@gmail.com>
2016-05-12 23:47:55 +0800

    add mongo env

--- BP3L/env.sh
+++ BP3L/env.sh
@@ -7,4 +7,3 @@ export BUILD_ROOT=`pwd`
 export MONGO_URL=mongodb://deviceLoadTestByiHealth:djsDfjweFHp33ew@120.131.8.26:27017,120.131.8.27:27017,120.131.8.46:27017/device-load-test?replicaSet=iHealthMongo
 export MONGO_OPLOG_URL=mongodb://deviceLoadTestByiHealth:djsDfjweFHp33ew@120.131.8.26:27017,120.131.8.27:27017,120.131.8.46:27017/local?authSource=device-load-test
 
-echo $PACKAGE_DIRS
