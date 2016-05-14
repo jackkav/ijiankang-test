a0e119c5c68e7c0881f26c1a9d9787228a2545e4
Jeffrey Fan <ennea8@gmail.com>
2016-05-14 17:26:35 +0800

    update auth code

--- BP3L/client/lib/BP3L/bp3l.js
+++ BP3L/client/lib/BP3L/bp3l.js
@@ -9,7 +9,7 @@ class BP3L extends EventEmitter {
 }
 this.BP3L = BP3L
 
-BP3L.appsecret = "148779a50cf3c1c06634c996b54def5e"
+
 
 
 BP3L.parseJSON = function (str, defaultValue) {
@@ -21,3 +21,34 @@ BP3L.parseJSON = function (str, defaultValue) {
 		return defaultValue || {}
 	}
 }
+
+BP3L.isIOS = function(){
+	var platform = window.device && window.device.platform.toLowerCase()
+	return  platform==='ios' ||  /iPhone|iPad|iPod/.test(navigator.platform)
+}
+
+BP3L.measureSecret= {
+	"ios":"ffd5a4a408e902acac70d7a8956f833c",
+	"android":"c8b6d55c7ac1e1eecfc1787bd244a214"
+}
+//BP3L.appsecret = "148779a50cf3c1c06634c996b54def5e"
+
+
+
+BP3L.initAppsecret = function(){
+
+	let appsecret
+	if(BP3L.isIOS()){
+		appsecret = BP3L.measureSecret.ios
+	}else{ //android
+		appsecret = BP3L.measureSecret.android
+	}
+
+	//expose
+	BP3L.appsecret = appsecret
+}
+
+//初始化验证字符串
+Meteor.startup(function(){
+	BP3L.initAppsecret()
+})
